ebb046c057b52af98225f0c8056e592c8ae04ced
naxvan <mahechalozada@gmail.com>
2021-12-10 20:47:49 -0500

    cambio de formato

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 3000;
+const path = require("path");
+
+app.set("view engine" , "ejs");
+
+app.use(`/css` , express.static('css'));
+app.use(`/img` , express.static('img'));
 
 
 app.listen(port ,() =>{ 
@@ -8,6 +15,38 @@ app.listen(port ,() =>{
 });
 
 
-app.get(`/` , (req,res) =>{
-    res.send("<h1>hola desde node<h1/>");
+
+
+app.get(`/inicio` , (req,res) =>{
+    res.render("index.ejs")
+})
+
+app.get(`/menuProductos` , (req,res) =>{
+    app.use('css' , express.static('css'))
+    res.render('MenuProductos.ejs')
+})
+
+
+app.get('/leche' , (req,res) =>{
+    res.render('leche.ejs');
+})
+
+app.get('/Huevos' , (req,res) =>{
+    res.render('Huevos.ejs');
+})
+
+app.get('/Fabuloso' , (req,res) =>{
+    res.render('Fabuloso.ejs');
+})
+
+app.get('/Papel' , (req,res) =>{
+    res.render('Papel.ejs')
+})
+
+app.get('/Salsa' , (req,res) =>{
+    res.render('Salsa.ejs')
+})
+
+app.get('/Pastas' , (req,res) =>{
+    res.render('Pastas.ejs')
 })
